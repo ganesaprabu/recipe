bfb585695e356919da78802cea0858fd19864365
ganesaprabu <ganesa.tech@gmail.com>
2019-01-23 06:29:21 +0530

    Added my project first time

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../Recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [];
+  constructor() {
+    var rec1 = new Recipe('Dosa', 'Plain Dosa', 'https://cdn.pixabay.com/photo/2016/10/25/13/42/indian-1768906_960_720.jpg');
+    var rec2 = new Recipe('Idly', 'Idly Sambar', 'https://cdn.pixabay.com/photo/2014/12/22/12/22/food-577224_960_720.jpg');
+    this.recipes.push(rec1);
+    this.recipes.push(rec2);
+  }
+
+  ngOnInit() {
+  }
+
+}
