dc470111bc187fd338899a465c509c64b1d977c8
ganesaprabu <41106620+ganesaprabu@users.noreply.github.com>
2019-01-28 11:05:35 +0530

    With Normal

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,7 +11,7 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
     new Ingredient('Potatoes', 12),
-    new Ingredient('Berry', 22)
+    new Ingredient('Normal Berry', 22)
   ];
   constructor() {
 
