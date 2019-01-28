874de7fbcd052fbaacb437eb306be535c231b533
ganesaprabu <41106620+ganesaprabu@users.noreply.github.com>
2019-01-28 10:56:43 +0530

    Updating from remote

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,7 +11,7 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
     new Ingredient('Potatoes', 12),
-    new Ingredient('Pumkin', 22)
+    new Ingredient('Black Berry', 22)
   ];
   constructor() {
 
